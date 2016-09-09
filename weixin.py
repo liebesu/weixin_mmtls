240341d9ec881c3824a6f1a17c3e5b04ab4862aa
liebesu <lichunjiang42@gmail.com>
2016-09-09 14:13:51 +0800

    UPDATE

--- weixin.py
+++ weixin.py
@@ -2,11 +2,16 @@ import requests
 import time
 import random
 import hashlib
-for i in range(1,1000):
+from multiprocessing import Pool
+
+def weixin(i):
     a=random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890')
     b=random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890')
     c=random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890')
-    strings=a+b+c
+    d=random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890')
+    e=random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890')
+    f=random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890')
+    strings=a+b+c+d+e+f
     m=hashlib.md5()
     m.update(strings)
     crestr=m.hexdigest()
@@ -14,6 +19,17 @@ for i in range(1,1000):
     r=requests.get(url)
     if r.status_code==200:
         print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!",url
+        z=open('have.txt','a')
+        z.write(url+"\n")
+        z.close()
     else:
         print r.status_code,strings,crestr
+
+
+if __name__=="__main__":
+    pool = Pool(processes=50)
+    pool.map(weixin,range(1,100000))
+    pool.join()
+    pool.close()
+
 
